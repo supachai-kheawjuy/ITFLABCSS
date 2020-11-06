264f4dd51a34349e4886b21c919ec4acbda99a60
Spicyz <bossthth1209mine@gmail.com>
2020-11-06 16:33:46 +0700

    some layout change

--- index.php
+++ index.php
@@ -8,22 +8,18 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <div class="container" style="align-content: center;">
         <div class="row">
-            <div class="col-sm-5">
-                <img src="img/creative/creative.jpg" class="img-fluid">
+            <div class=" col-sm-6">
+                <img class="img-fluid" src="img/creative/creative.jpg" style="height: 100%;width: 100%" />
+            </div>
+            <div class=" col-sm-3">
+                <img src="img/creative/creative5.jpg" class="img-fluid" style="margin-bottom: 30px;"/>
+                <img src="img/life/life9.jpg" class="img-fluid" style="margin-bottom: 30px;"/>
+                <img src="img/staffs/staff4.jpg" class="img-fluid" style="margin-bottom: 30px;"/>
             </div>
             <div class="col-sm-3">
-                <img src="img/creative/creative5.jpg" class="img-fluid">
-                <img src="img/life/life9.jpg" class="img-fluid">
-                <img src="img/staffs/staff4.jpg" class="img-fluid">
+                <img src="img/creative/creative2.jpg" class="img-fluid" style="margin-bottom: 30px; height: 60%;width: 100%;"/>
+                <img class="img-fluid" src="img/creative/creative8.jpg" style="width: 255px; height: 255px;"/>
             </div>
-            <div class="col-sm-4">
-                <img src="img/creative/creative2.jpg" class="img-fluid">
-                <img src="img/creative/creative8.jpg" class="img-fluid">
-            </div>
-        </div>
-        <div class="row">
-            <a href="/card.html" class="btn btn-primary">Card</a>
-            <a href="https://www.google.com" class="btn btn-danger">Google</a>
         </div>
     </div>
 </head>
